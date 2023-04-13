eecc6dc09aa00b10c92a655e99e32d299188badb
Roman <romamail.ru365@gmail.com>
2023-04-13 17:31:24 +0500

    разработка адаптивной страницы родителя

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,49 @@ import {CardBasket} from "./Components/BasketCard/CardBasket";
 import "./index.css"
 import {FilterFoodItem} from "./Components/FilterFoodItem/FilterFoodItem";
 import {LeftMenu} from "./Components/LeftMenuItem/LeftMenu";
+import {AdaptNavbar} from "./Components/TestAdaptiv/AdaptNavbar";
 
 
 function App() {
   return (
-      <html lang="en">
-      <head>
-          <meta charSet="UTF-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Школьное питание</title>
-      </head>
-      <body style={{background:"#F8F8F8"}}>
-        <Navibar/>
-        <div style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "20px",
-            width: "1119px",
-            marginTop: "70px",
-            marginLeft: "auto",
-            marginRight: "auto"
-        }}>
-            <LeftMenu/>
-            <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-                <FilterFoodItem/>
-                <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-                    <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
-                        <CardFood/>
-                        <CardFood/>
-                        <CardFood/>
-                    </div>
-                    <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
-                        <CardFood/>
-                        <CardFood/>
-                        <CardFood/>
-                    </div>
-                </div>
-            </div>
-            <CardBasket/>
-        </div>
-      </body>
-      </html>
+      <AdaptNavbar></AdaptNavbar>
+      // <html lang="en">
+      // <head>
+      //     <meta charSet="UTF-8"/>
+      //     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      //     <title>Школьное питание</title>
+      // </head>
+      // <body style={{background:"#F8F8F8"}}>
+      //   <Navibar/>
+      //   <div style={{
+      //       display: "flex",
+      //       flexDirection: "row",
+      //       gap: "20px",
+      //       width: "1119px",
+      //       marginTop: "70px",
+      //       marginLeft: "auto",
+      //       marginRight: "auto"
+      //   }}>
+      //       <LeftMenu/>
+      //       <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+      //           <FilterFoodItem/>
+      //           <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+      //               <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
+      //                   <CardFood/>
+      //                   <CardFood/>
+      //                   <CardFood/>
+      //               </div>
+      //               <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
+      //                   <CardFood/>
+      //                   <CardFood/>
+      //                   <CardFood/>
+      //               </div>
+      //           </div>
+      //       </div>
+      //       <CardBasket/>
+      //   </div>
+      // </body>
+      // </html>
   );
 }
 
