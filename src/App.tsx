14afcec44127ce91d146603897bc8fa4396eed5e
Roman <romamail.ru365@gmail.com>
2023-05-07 18:36:43 +0500

    рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css"
 import {TeacherPage} from "./Pages/TeacherPage";
-import {ParentPage} from "./Pages/ParentPage";
 
 
 function App() {
   return (
-      <ParentPage/>
+      <TeacherPage/>
   );
 }
 
