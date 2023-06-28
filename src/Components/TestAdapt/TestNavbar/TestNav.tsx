80ed08d28694e6dabfe579910770f3f8c35f5bb4
Roman <romamail.ru365@gmail.com>
2023-06-28 17:06:25 +0500

    navbar: рефакторинг классов

--- src/Components/TestAdapt/TestNavbar/TestNav.tsx
+++ src/Components/TestAdapt/TestNavbar/TestNav.tsx
@@ -4,6 +4,7 @@ import menu from "../AdaptNavbar/Img/navbarToggler.png";
 import logo from "../../Navbar/Img/Mask group.png";
 import exit from "../../Navbar/Img/r_m_exit.png";
 import basket from "../AdaptNavbar/Img/VectorBasket.png";
+import styles from "./TestNav.module.scss";
 
 
 interface IProps {
@@ -14,6 +15,7 @@ interface IState{
     navigation?: any
     buttons?: any
 }
+
 export class TestNav extends React.Component<IProps, IState>{
     constructor(props: IProps) {
         super(props)
@@ -38,22 +40,22 @@ export class TestNav extends React.Component<IProps, IState>{
 
         return(
             <Disclosure>
-                <nav className={"bg-white"}>
-                    <div className={"flex mx-auto max-w-nav-main-adapt h-28 2xl:h-32 xl:h-32 lg:h-32 lg:max-w-nav-main md:max-w-nav-main-medium"}>
-                        <Disclosure.Button className={"border-0 bg-white h-[18px] ml-8 mt-[67px] md:hidden focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"}>
+                <nav className={styles.nav}>
+                    <div className={styles.mainBlock}>
+                        <Disclosure.Button className={styles.dropDownMenuButton}>
                             <img src={menu} alt={""}/>
                         </Disclosure.Button>
-                        <div className={"flex flex-row mx-auto mt-12 lg:mx-auto lg:ml-11 md:mx-0 md:my-auto md:ml-4 sm:mx-auto sm:mt-12"}>
-                            <div className={"flex flex-row my-auto gap-1 xl:min-w-[280px] lg:min-w-[270px] md:gap-3 md:min-w-[177px] sm:gap-2"}>
-                                <div><img src={logo} alt={""}/></div>
-                                <p className={"text-black text-[20px] leading-6 font-bold w-[103px]"}>Школьное Питание</p>
+                        <div className={styles.leftBlock}>
+                            <div className={styles.logo}>
+                                <img src={logo} alt={""}/>
+                                <p className={styles.textLogo}>Школьное Питание</p>
                             </div>
-                            <div className={"hidden xl:gap-10 lg:gap-4 md:flex md:gap-2 md:px-2 md:py-[10px] md:h-[62px]"}>
+                            <div className={styles.mainButtons}>
                                 {this.state.buttons.map((btn: any) => (
                                     <a
                                         href={btn.href}
-                                        className={changeClasses(btn.current ? "text-white text-[18px] font-semibold rounded-[10px] bg-[#F05654] text-center py-[10px] px-[20px] leading-5" :
-                                        "text-black text-[18px] font-semibold text-center py-[10px] px-[20px] leading-5")}
+                                        className={changeClasses(btn.current ? `${styles.baseButton} ${styles.redButton}` :
+                                            `${styles.baseButton}`)}
                                         onClick={() => {
                                             const newCurrentButtons = [...this.state.buttons]
                                             newCurrentButtons.find((item) => item.current === true ? item.current = false : item.current = false)
@@ -67,22 +69,22 @@ export class TestNav extends React.Component<IProps, IState>{
                                 ))}
                             </div>
                         </div>
-                        <div className={"hidden lg:mr-10 md:flex md:flex-row md:my-auto md:py-[10px] md:gap-3 md:mr-4"}>
-                            <a href={""} className={"text-black text-[18px] font-semibold p-[10px]"}>Профиль</a>
-                            <a href={""} className={"w-6 h-6 my-auto"}><img src={exit} alt={""}/></a>
+                        <div className={styles.rightBlock}>
+                            <a href={"#"} className={styles.profileButton}>Профиль</a>
+                            <a href={"#"} className={styles.exitButton}><img src={exit} alt={""}/></a>
                         </div>
-                        <a href={""} className={"w-[30px] h-[30px] mt-[61px] mr-8 md:hidden"}><img src={basket} alt={""}></img></a>
+                        <a href={"#"} className={styles.basketButton}><img src={basket} alt={""}></img></a>
                     </div>
                 </nav>
-                <Disclosure.Panel className={"md:hidden"}>
-                    <div className={"flex flex-col gap-5 pt-[30px] px-4 mi-w-[728px]"}>
+                <Disclosure.Panel className={styles.dropDownMenu}>
+                    <div className={styles.dropDownButtons}>
                         {this.state.navigation.map((currItem: any) => (
                             <Disclosure.Button
                                 key={currItem.name}
                                 as={"a"}
                                 href={currItem.href}
-                                className={changeClasses(currItem.current ? "text-white text-[18px] font-semibold rounded-[10px] bg-[#F05654] h-[60px] text-center pt-[15px]" :
-                                    "text-black text-[18px] font-semibold rounded-[10px] h-[60px] bg-white text-center pt-[15px]"
+                                className={changeClasses(currItem.current ? `${styles.dropDownBaseButton} ${styles.dropDownRedButton}` :
+                                    `${styles.dropDownBaseButton}`
                                 )}
                                 aria-current={currItem.current ? 'page' : undefined}
                                 onClick={() => {
@@ -101,5 +103,4 @@ export class TestNav extends React.Component<IProps, IState>{
             </Disclosure>
         );
     }
-
 }
